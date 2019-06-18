e08779a8fbe013652cdcfb5cc446f7cee4fa9895
BULLXM123 <602424123@qq.com>
2019-06-18 16:32:50 +0800

    第五次提交

---
+++ code/cms_project/src/components/Common/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="cms_s_header">
+            <h1 @click="goback" >&nbsp&#60</h1>
+            <h1 class="page_title">{{title}}</h1>
+        </div>
+</template>
+
+<script>
+export default {
+    props:['title'],
+    name:'nav-bar',
+    methods: {
+        goback() {
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .cms_s_header {
+        /* display: inline-block; */
+        margin-top:40px;
+        /* height: 40px; */
+        background-color: #f7f7f7;
+    }
+    .page_title{
+        margin-left: 25%;
+    }
+    h1 {
+        display:inline-block;
+        height:40px;
+        line-height: 40px;
+        margin-top: 0px;
+        margin-bottom: 0px;
+    }
+</style>
